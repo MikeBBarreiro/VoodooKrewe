7062233138096e4bdcdbe15e3b0015ca7fa05f3d
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2016-06-02 00:55:04 -0500

    Music Player Added with scr scrolrolling volume and new images from players inclueded for background landing

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -5,7 +5,11 @@ window.onload = function(){
     'images/VKplayerlandings/reece00.jpg',
     'images/VKplayerlandings/TekkVKPartical.jpg',
     'images/VKplayerlandings/matt00.jpg',
-    'images/VKplayerlandings/jeremy00.jpeg'
+    'images/VKplayerlandings/jeremy00.jpeg',
+    'images/VKplayerlandings/brodey00.jpg',
+    'images/VKplayerlandings/blake00.jpg',
+    'images/VKplayerlandings/lj00.jpg',
+    'images/VKplayerlandings/andrew00.jpg'
   ];
   var today = new Date();
   var day = today.getDate();
@@ -17,6 +21,36 @@ window.onload = function(){
 var toggleFullImage = function(){
   $('#content2').toggle(200);
 }
+
+window.playSound = function(){
+	var audio = document.getElementById("audio");
+	audio.volume = 0.1;
+	audio.play();
+}();
+
+var pauseMusic = function(){audio.pause();}
+$('#musicPlayer').click(function() {
+  $('#musicPlayer').removeClass('bounceInLeft');
+  $('#musicPlayer').addClass('bounceOutLeft');
+});
+
+
+$(window).scroll(function(){
+  // console.log($(window).scrollTop());
+  if($(window).scrollTop() > 265 && $(window).scrollTop() < 300){
+     audio.volume = 0.05;
+  }else if($(window).scrollTop() > 300 && $(window).scrollTop() < 465){
+    audio.volume = 0.04;
+  }else if($(window).scrollTop() > 465 && $(window).scrollTop() < 565){
+    audio.volume = 0.03;
+  }else if($(window).scrollTop() > 565 && $(window).scrollTop() < 665){
+    audio.volume = 0.02;
+  }else if($(window).scrollTop() > 665 && $(window).scrollTop() < 765){
+    audio.volume = 0.01;
+  }else if($(window).scrollTop() > 795){
+    audio.volume = 0;
+  }
+})
 
 // $(".viewImage").click( function(event){
 // 	event.preventDefault();
